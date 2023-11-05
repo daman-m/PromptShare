ec6f1f28e229ffb679c586bab78e5647f74d7d06
damanM <daman.m@outlook.com>
2023-11-05 13:05:35 -0500

    set up db with create post and auth functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,46 +5,48 @@ import User from "@models/user";
 
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    async session({session}) {
-        const sessionUser = await User.findOne({
-            email:session.user.email
-        })
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      })
 
-        session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-        return session;
+      return session;
     },
     async signIn({ profile }) {
-        try {
-            await connectToDB();
+      try {
+        await connectToDB();
 
-            //check if a user alredy exists
-            const userExists = await User.findOne({
-                email: profile.email
-            })
+        //check if a user alredy exists
+        const userExists = await User.findOne({
+          email: profile.email
+        })
 
-            // if not create new user
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ","").toLowerCase(),
-                    image: profile.picture
-                })
-            } 
-
-            return true
-        } catch (error) {
-            console.error(error)
-            return false
+        // if not create new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          })
         }
 
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+
     }
+  }
 })
 
-export {  handler as GET, handler as POST }
+export { handler as GET, handler as POST }
